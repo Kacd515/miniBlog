1787df1689ffae270d1af72713dcf63aab22ea33
Michał Makaruk <makaruk.michal@gmail.com>
2018-07-26 11:43:31 +0200

    add post and display all posts

---
+++ src/main/java/pl/akademiakodu/miniBlog/dao/PostDao.java
@@ -0,0 +1,8 @@
+package pl.akademiakodu.miniBlog.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import pl.akademiakodu.miniBlog.model.Post;
+
+public interface PostDao extends CrudRepository<Post,Long> {
+
+}
